1f452866ceb3c8e7ff0c2599e36aef4f3543cee7
yukihane <yukihane.feather@gmail.com>
2017-08-15 21:06:19 +0900

    「use でモジュールをインポートする」

--- 04_25crates_and_modules/src/main.rs
+++ 04_25crates_and_modules/src/main.rs
@@ -1,11 +1,10 @@
 extern crate phrases;
 
+use phrases::english::{farewells, greetings};
+
 fn main() {
-    println!("Hello in English: {}", phrases::english::greetings::hello());
-    println!(
-        "Goodbye in English: {}",
-        phrases::english::farewells::goodbye()
-    );
+    println!("Hello in English: {}", greetings::hello());
+    println!("Goodbye in English: {}", farewells::goodbye());
     //
     //    println!(
     //        "Hello in Japanese: {}",
