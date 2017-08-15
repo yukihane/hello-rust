eb6a60c6271939e244d1325c32416b3102fd557d
yukihane <yukihane.feather@gmail.com>
2017-08-15 14:59:41 +0900

    「モジュールを定義する」

--- 04_25crates_and_modules/src/lib.rs
+++ 04_25crates_and_modules/src/lib.rs
@@ -1,6 +1,11 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-    }
+mod english {
+    mod greetings {}
+
+    mod farewells {}
+}
+
+mod japanese {
+    mod greetings {}
+
+    mod farewells {}
 }
