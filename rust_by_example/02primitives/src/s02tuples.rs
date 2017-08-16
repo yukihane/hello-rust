809eb141964abfc9c572f488f4d812b14303ff78
yukihane <yukihane.feather@gmail.com>
2017-08-16 16:48:38 +0900

    2.2. タプル 演習

--- rust_by_example/02primitives/src/s02tuples.rs
+++ rust_by_example/02primitives/src/s02tuples.rs
@@ -1,10 +1,22 @@
-pub fn reverse(pair: (i32, bool)) -> (bool, i32) {
+use std::fmt;
+
+fn reverse(pair: (i32, bool)) -> (bool, i32) {
     let (p1, p2) = pair;
     (p2, p1)
 }
 
+fn transpose(m: Matrix) -> Matrix {
+    Matrix(m.0, m.2, m.1, m.3)
+}
+
 #[derive(Debug)]
 struct Matrix(f32, f32, f32, f32);
+
+impl fmt::Display for Matrix {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "( {} {} )\n( {} {} )", self.0, self.1, self.2, self.3)
+    }
+}
 
 pub fn main() {
     let long_tuple = (
@@ -42,5 +54,6 @@ pub fn main() {
     println!("{:?}, {:?}, {:?}, {:?}", a, b, c, d);
 
     let matrix = Matrix(1.1, 1.2, 2.1, 2.2);
-    println!("{:?}", matrix);
+    println!("{}", matrix);
+    println!("Transpose:\n{}", transpose(matrix));
 }
