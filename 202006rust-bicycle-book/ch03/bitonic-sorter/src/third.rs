f92ed0bda8ee0463b92b219c9f3320596b827a33
yukihane <yukihane.feather@gmail.com>
2020-06-29 21:36:05 +0900

    テストケース追加

--- 202006rust-bicycle-book/ch03/bitonic-sorter/src/third.rs
+++ 202006rust-bicycle-book/ch03/bitonic-sorter/src/third.rs
@@ -44,7 +44,7 @@ fn compare_and_swap<T: Ord>(x: &mut [T], up: bool) {
 
 #[cfg(test)]
 mod tests {
-    use super::sort;
+    use super::{is_power_of_two, sort, sort_by};
     use crate::SortOrder::*;
 
     #[test]
@@ -99,5 +99,59 @@ mod tests {
     fn sort_to_fail() {
         let mut x = vec![10, 30, 11];
         assert!(sort(&mut x, &Ascending).is_err());
+    }
+
+    struct Student {
+        first_name: String,
+        last_name: String,
+        age: u8,
+    }
+
+    impl Student {
+        fn new(first_name: &str, last_name: &str, age: u8) -> Self {
+            Self {
+                first_name: first_name.to_string(),
+                last_name: last_name.to_string(),
+                age,
+            }
+        }
+    }
+
+    #[test]
+    fn sort_students_by_age_ascendint() {
+        let taro = Student::new("Talo", "Yamada", 16);
+        let hanako = Student::new("Hanako", "Yamada", 14);
+        let kyoko = Student::new("kyoko", "Ito", 15);
+        let ryosuke = Student::new("Ryosuke", "Hayashi", 17);
+
+        let mut x = vec![&taro, &hanako, &kyoko, &ryosuke];
+
+        let expected = vec![&hanako, &kyoko, &taro, &ryosuke];
+
+        assert_eq!(sort_by(&mut x, &|a, b| a.age.cmp(&b.age)), Ok(()));
+
+        assert_eq!(x, expected);
+    }
+
+    #[test]
+    fn sort_students_by_name_ascendint() {
+        let taro = Student::new("Talo", "Yamada", 16);
+        let hanako = Student::new("Hanako", "Yamada", 14);
+        let kyoko = Student::new("kyoko", "Ito", 15);
+        let ryosuke = Student::new("Ryosuke", "Hayashi", 17);
+
+        let mut x = vec![&taro, &hanako, &kyoko, &ryosuke];
+
+        let expected = vec![&ryosuke, &kyoko, &hanako, &taro];
+
+        assert_eq!(
+            sort_by(&mut x, &|a, b| a
+                .last_name
+                .comp(&b.last_name)
+                .then_with(|| a.first_name.cmp(&b.first_name))),
+            Ok(())
+        );
+
+        assert_eq!(x, expected);
     }
 }
