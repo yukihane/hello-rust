803b6fa99e519386582da3cab9361ccc38dc9910
yukihane <yukihane.feather@gmail.com>
2020-06-29 09:26:00 +0900

    Ordトレイト境界設定

--- 202006rust-bicycle-book/ch03/bitonic-sorter/src/first.rs
+++ 202006rust-bicycle-book/ch03/bitonic-sorter/src/first.rs
@@ -1,4 +1,4 @@
-pub fn sort(x: &mut [u32], up: bool) {
+pub fn sort<T: Ord>(x: &mut [T], up: bool) {
     if x.len() > 1 {
         let mid_point = x.len() / 2;
         sort(&mut x[..mid_point], true);
@@ -7,7 +7,7 @@ pub fn sort(x: &mut [u32], up: bool) {
     }
 }
 
-fn sub_sort(x: &mut [u32], up: bool) {
+fn sub_sort<T: Ord>(x: &mut [T], up: bool) {
     if x.len() > 1 {
         compare_and_swap(x, up);
         let mid_point = x.len() / 2;
@@ -16,7 +16,7 @@ fn sub_sort(x: &mut [u32], up: bool) {
     }
 }
 
-fn compare_and_swap(x: &mut [u32], up: bool) {
+fn compare_and_swap<T: Ord>(x: &mut [T], up: bool) {
     let mid_point = x.len() / 2;
     for i in 0..mid_point {
         if (x[i] > x[mid_point + i]) == up {
