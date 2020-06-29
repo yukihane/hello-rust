c24c7dfd3bf118dc4fbd17e9fe9fe86132017dce
yukihane <yukihane.feather@gmail.com>
2020-06-29 11:28:57 +0900

    enum導入

--- 202006rust-bicycle-book/ch03/bitonic-sorter/src/second.rs
+++ 202006rust-bicycle-book/ch03/bitonic-sorter/src/second.rs
@@ -1,8 +1,17 @@
-pub fn sort<T: Ord>(x: &mut [T], up: bool) {
+use super::SortOrder;
+
+pub fn sort<T: Ord>(x: &mut [T], order: &SortOrder) {
+    match *order {
+        SortOrder::Ascending => do_sort(x, true),
+        SortOrder::Descending => do_sort(x, false),
+    }
+}
+
+fn do_sort<T: Ord>(x: &mut [T], up: bool) {
     if x.len() > 1 {
         let mid_point = x.len() / 2;
-        sort(&mut x[..mid_point], true);
-        sort(&mut x[mid_point..], false);
+        do_sort(&mut x[..mid_point], true);
+        do_sort(&mut x[mid_point..], false);
         sub_sort(x, up);
     }
 }
@@ -28,12 +37,13 @@ fn compare_and_swap<T: Ord>(x: &mut [T], up: bool) {
 #[cfg(test)]
 mod tests {
     use super::sort;
+    use crate::SortOrder::*;
 
     #[test]
     fn sort_u32_ascending() {
         let mut x: Vec<u32> = vec![10, 30, 11, 20, 4, 330, 21, 110];
 
-        sort(&mut x, true);
+        sort(&mut x, &Ascending);
 
         assert_eq!(x, vec![4, 10, 11, 20, 21, 30, 110, 330]);
     }
@@ -42,7 +52,7 @@ mod tests {
     fn sort_u32_descending() {
         let mut x: Vec<u32> = vec![10, 30, 11, 20, 4, 330, 21, 110];
 
-        sort(&mut x, false);
+        sort(&mut x, &Descending);
 
         assert_eq!(x, vec![330, 110, 30, 21, 20, 11, 10, 4]);
     }
@@ -60,7 +70,7 @@ mod tests {
             "GC",
         ];
 
-        sort(&mut x, true);
+        sort(&mut x, &Ascending);
 
         assert_eq!(
             x,
