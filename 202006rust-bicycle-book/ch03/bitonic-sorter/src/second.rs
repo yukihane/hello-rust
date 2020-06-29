f6eb964499e7f2321cddcab0666c416e1d23cedc
yukihane <yukihane.feather@gmail.com>
2020-06-29 17:25:27 +0900

    警告解消

--- 202006rust-bicycle-book/ch03/bitonic-sorter/src/second.rs
+++ 202006rust-bicycle-book/ch03/bitonic-sorter/src/second.rs
@@ -51,7 +51,7 @@ mod tests {
     fn sort_u32_ascending() {
         let mut x: Vec<u32> = vec![10, 30, 11, 20, 4, 330, 21, 110];
 
-        sort(&mut x, &Ascending);
+        assert_eq!(sort(&mut x, &Ascending), Ok(()));
 
         assert_eq!(x, vec![4, 10, 11, 20, 21, 30, 110, 330]);
     }
@@ -60,7 +60,7 @@ mod tests {
     fn sort_u32_descending() {
         let mut x: Vec<u32> = vec![10, 30, 11, 20, 4, 330, 21, 110];
 
-        sort(&mut x, &Descending);
+        assert_eq!(sort(&mut x, &Descending), Ok(()));
 
         assert_eq!(x, vec![330, 110, 30, 21, 20, 11, 10, 4]);
     }
@@ -78,7 +78,7 @@ mod tests {
             "GC",
         ];
 
-        sort(&mut x, &Ascending);
+        assert_eq!(sort(&mut x, &Ascending), Ok(()));
 
         assert_eq!(
             x,
