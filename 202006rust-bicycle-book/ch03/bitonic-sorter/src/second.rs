7de20a91ac5cbc40d7babca3206be2453fa9b86d
yukihane <yukihane.feather@gmail.com>
2020-06-29 09:29:49 +0900

    generics版とそうじゃないのはファイルを分けていたのでそれに従う。

--- 202006rust-bicycle-book/ch03/bitonic-sorter/src/second.rs
+++ 202006rust-bicycle-book/ch03/bitonic-sorter/src/second.rs
@@ -1,4 +1,4 @@
-pub fn sort(x: &mut [u32], up: bool) {
+pub fn sort<T: Ord>(x: &mut [T], up: bool) {
     if x.len() > 1 {
         let mid_point = x.len() / 2;
         sort(&mut x[..mid_point], true);
@@ -7,7 +7,7 @@ pub fn sort(x: &mut [u32], up: bool) {
     }
 }
 
-fn sub_sort(x: &mut [u32], up: bool) {
+fn sub_sort<T: Ord>(x: &mut [T], up: bool) {
     if x.len() > 1 {
         compare_and_swap(x, up);
         let mid_point = x.len() / 2;
@@ -16,7 +16,7 @@ fn sub_sort(x: &mut [u32], up: bool) {
     }
 }
 
-fn compare_and_swap(x: &mut [u32], up: bool) {
+fn compare_and_swap<T: Ord>(x: &mut [T], up: bool) {
     let mid_point = x.len() / 2;
     for i in 0..mid_point {
         if (x[i] > x[mid_point + i]) == up {
@@ -31,7 +31,7 @@ mod tests {
 
     #[test]
     fn sort_u32_ascending() {
-        let mut x = vec![10, 30, 11, 20, 4, 330, 21, 110];
+        let mut x: Vec<u32> = vec![10, 30, 11, 20, 4, 330, 21, 110];
 
         sort(&mut x, true);
 
@@ -40,10 +40,40 @@ mod tests {
 
     #[test]
     fn sort_u32_descending() {
-        let mut x = vec![10, 30, 11, 20, 4, 330, 21, 110];
+        let mut x: Vec<u32> = vec![10, 30, 11, 20, 4, 330, 21, 110];
 
         sort(&mut x, false);
 
         assert_eq!(x, vec![330, 110, 30, 21, 20, 11, 10, 4]);
+    }
+
+    #[test]
+    fn sort_str_ascending() {
+        let mut x = vec![
+            "Rust",
+            "is",
+            "fast",
+            "and",
+            "memory-efficient",
+            "with",
+            "no",
+            "GC",
+        ];
+
+        sort(&mut x, true);
+
+        assert_eq!(
+            x,
+            vec![
+                "GC",
+                "Rust",
+                "and",
+                "fast",
+                "is",
+                "memory-efficient",
+                "no",
+                "with"
+            ]
+        );
     }
 }
