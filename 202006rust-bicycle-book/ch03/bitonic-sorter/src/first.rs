1840b6d16886c2407d378f5c1ba7584d7deeaa24
yukihane <yukihane.feather@gmail.com>
2020-06-29 09:03:43 +0900

    テストケース追加(文字列ソート)

--- 202006rust-bicycle-book/ch03/bitonic-sorter/src/first.rs
+++ 202006rust-bicycle-book/ch03/bitonic-sorter/src/first.rs
@@ -31,7 +31,7 @@ mod tests {
 
     #[test]
     fn sort_u32_ascending() {
-        let mut x = vec![10, 30, 11, 20, 4, 330, 21, 110];
+        let mut x: Vec<u32> = vec![10, 30, 11, 20, 4, 330, 21, 110];
 
         sort(&mut x, true);
 
@@ -40,10 +40,40 @@ mod tests {
 
     #[test]
     fn sort_u32_descending() {
-        let mut x = vec![10, 30, 11, 20, 4, 330, 21, 110];
+        let mut x: Vec<u32> = vec![10, 30, 11, 20, 4, 330, 21, 110];
 
         sort(&mut x, false);
 
         assert_eq!(x, vec![330, 110, 30, 21, 20, 11, 10, 4]);
+    }
+
+    #[test]
+    fn sort_str_ascending() {
+        let mut x = vec![
+            "Rust",
+            "is",
+            "fast",
+            "and",
+            "memory-efficient",
+            "with",
+            "no",
+            "GC",
+        ];
+
+        sort(&mut x, true);
+
+        assert_eq!(
+            x,
+            vec![
+                "GC",
+                "Rust",
+                "and",
+                "fast",
+                "is",
+                "memory-efficient",
+                "no",
+                "with"
+            ]
+        );
     }
 }
