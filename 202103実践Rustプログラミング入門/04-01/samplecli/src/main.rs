3c2071ac4ef9524d81a33efa932d97d14560a46f
yukihane <yukihane.feather@gmail.com>
2021-03-14 13:14:09 +0900

    関数に切り出し

--- 202103実践Rustプログラミング入門/04-01/samplecli/src/main.rs
+++ 202103実践Rustプログラミング入門/04-01/samplecli/src/main.rs
@@ -27,13 +27,16 @@ fn main() {
     if let Some(path) = opts.formula_file {
         let f = File::open(path).unwrap();
         let reader = BufReader::new(f);
-
-        for line in reader.lines() {
-            let line = line.unwrap();
-            println!("{}", line);
-        }
+        run(reader, opts.verbose);
     } else {
         // ファイルを指定しなかった場合
         println!("No file is specified");
+    }
+}
+
+fn run(reader: BufReader<File>, verbose: bool) {
+    for line in reader.lines() {
+        let line = line.unwrap();
+        println!("{}", line);
     }
 }
