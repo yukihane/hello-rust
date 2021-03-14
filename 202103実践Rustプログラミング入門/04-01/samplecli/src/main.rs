0b59855afdfe4bd8848cc8ede402c54fad933ad5
yukihane <yukihane.feather@gmail.com>
2021-03-14 11:29:41 +0900

    引数処理

--- 202103実践Rustプログラミング入門/04-01/samplecli/src/main.rs
+++ 202103実践Rustプログラミング入門/04-01/samplecli/src/main.rs
@@ -1,3 +1,30 @@
+use clap::{App, Arg};
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("My RPN program")
+        .version("1.0.0")
+        .author("Your name")
+        .about("Super awesome RPN calculator")
+        .arg(
+            Arg::new("formula_file")
+                .about("Formulas written in RPN")
+                .value_name("FILE")
+                .index(1)
+                .required(false),
+        )
+        .arg(
+            Arg::new("verbose")
+                .about("Sets the level of verbosity")
+                .short('v')
+                .long("verbose")
+                .required(false),
+        )
+        .get_matches();
+
+    match matches.value_of("formula_file") {
+        Some(file) => println!("File specified: {}", file),
+        None => println!("No file specified."),
+    }
+    let verbose = matches.is_present("verbose");
+    println!("Is verbosity specified?: {}", verbose);
 }
