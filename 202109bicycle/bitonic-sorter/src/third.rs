e9c51bedb50cd61c373680c5e8ac85644ec32938
yukihane <yukihane.feather@gmail.com>
2021-09-29 05:25:51 +0900

    3.5 まで

--- 202109bicycle/bitonic-sorter/src/third.rs
+++ 202109bicycle/bitonic-sorter/src/third.rs
@@ -66,7 +66,19 @@ where
 #[cfg(test)]
 mod tests {
     use super::{sort, sort_by};
+    use crate::utils::{is_sorted_ascending, is_sorted_descending, new_u32_vec};
     use crate::SortOrder::*;
+
+    #[test]
+    fn sort_u32_large() {
+        let mut x = new_u32_vec(65536);
+        assert_eq!(sort(&mut x, &Ascending), Ok(()));
+        assert!(is_sorted_ascending(&x));
+
+        let mut x = new_u32_vec(65536);
+        assert_eq!(sort(&mut x, &Descending), Ok(()));
+        assert!(is_sorted_descending(&x));
+    }
 
     #[derive(Debug, PartialEq)]
     struct Student {
