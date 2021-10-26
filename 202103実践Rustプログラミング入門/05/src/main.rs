7dd2d0df20e04090900a6eff7e9fb1b30c5979d1
yukihane <yukihane.feather@gmail.com>
2021-10-26 16:45:04 +0900

    template engine を利用して html をレンダリング

--- 202103実践Rustプログラミング入門/05/src/main.rs
+++ 202103実践Rustプログラミング入門/05/src/main.rs
@@ -1,10 +1,44 @@
-use actix_web::{get, App, HttpResponse, HttpServer};
+use actix_web::{get, App, HttpResponse, HttpServer, ResponseError};
+use askama::Template;
+use thiserror::Error;
+
+struct TodoEntry {
+    id: u32,
+    text: String,
+}
+
+#[derive(Template)]
+#[template(path = "index.html")]
+struct IndexTemplate {
+    entries: Vec<TodoEntry>,
+}
+
+#[derive(Error, Debug)]
+enum MyError {
+    #[error("Failed to render HTML")]
+    AskamaError(#[from] askama::Error),
+}
+
+impl ResponseError for MyError {}
 
 #[get("/")]
-async fn index() -> Result<HttpResponse, actix_web::Error> {
-    let response_body = "Hello, world!";
+async fn index() -> Result<HttpResponse, MyError> {
+    let mut entries = Vec::new();
+    entries.push(TodoEntry {
+        id: 1,
+        text: "First entry".to_string(),
+    });
+    entries.push(TodoEntry {
+        id: 2,
+        text: "Second entry".to_string(),
+    });
 
-    Ok(HttpResponse::Ok().body(response_body))
+    let html = IndexTemplate { entries };
+    let response_body = html.render()?;
+
+    Ok(HttpResponse::Ok()
+        .content_type("text/html")
+        .body(response_body))
 }
 
 #[actix_web::main]
