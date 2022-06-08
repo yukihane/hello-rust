35a75c5833367fc0133bbde3e08dcf9347d6350b
yukihane <yukihane.feather@gmail.com>
2022-06-09 06:38:56 +0900

    display

---
+++ thebook/202206/s05/src/bin/trait.rs
@@ -0,0 +1,14 @@
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+fn main() {
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+
+    // rect1は{}です
+    println!("rect1 is {}", rect1);
+}
