cab95665010a3d0c85eab2c2adc91652940d7a2b
yukihane <yukihane.feather@gmail.com>
2023-07-21 01:05:48 +0900

    2周目の三角形をハードコード

--- book-wasm/walk-the-dog/src/lib.rs
+++ book-wasm/walk-the-dog/src/lib.rs
@@ -34,6 +34,9 @@ pub fn main_js() -> Result<(), JsValue> {
         .unwrap();
 
     draw_triangle(&context, [(300.0, 0.0), (0.0, 600.0), (600.0, 600.0)]);
+    draw_triangle(&context, [(300.0, 0.0), (150.0, 300.0), (450.0, 300.0)]);
+    draw_triangle(&context, [(150.0, 300.0), (0.0, 600.0), (300.0, 600.0)]);
+    draw_triangle(&context, [(450.0, 300.0), (300.0, 600.0), (600.0, 600.0)]);
 
     Ok(())
 }
