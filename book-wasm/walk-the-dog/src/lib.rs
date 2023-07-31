dff8c23baf3634b44113dd43c319bc0d5477cd77
yukihane <yukihane.feather@gmail.com>
2023-08-01 05:47:01 +0900

    画像のロードを試す(が表示されない)

--- book-wasm/walk-the-dog/src/lib.rs
+++ book-wasm/walk-the-dog/src/lib.rs
@@ -35,6 +35,10 @@ pub fn main_js() -> Result<(), JsValue> {
         .dyn_into::<web_sys::CanvasRenderingContext2d>()
         .unwrap();
 
+    let image = web_sys::HtmlImageElement::new().unwrap();
+    image.set_src("Idle (1).png");
+    context.draw_image_with_html_image_element(&image, 0.0, 0.0);
+
     sierpinski(
         &context,
         [(300.0, 0.0), (0.0, 600.0), (600.0, 600.0)],
