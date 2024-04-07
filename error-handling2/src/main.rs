3edb25ea8cd3379460697018a743980c17998a84
yukihane <yukihane.feather@gmail.com>
2024-04-07 23:33:20 +0900

    最初のコード

--- error-handling2/src/main.rs
+++ error-handling2/src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+use std::error::Error;
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::BufReader;
+
+fn count_words<R: Read>(input: &mut R) -> Result<u32, Box<dyn Error>> {
+    let reader = BufReader::new(input);
+    let mut wordcount = 0;
+    for line in reader.lines() {
+        for _word in line?.split_whitespace() {
+            wordcount += 1;
+        }
+    }
+    Ok(wordcount)
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    for filename in env::args().skip(1).collect::<Vec<String>>() {
+        let mut reader = File::open(&filename)?;
+
+        let wordcount = count_words(&mut reader)?;
+        println!("{} {}", wordcount, filename);
+    }
+    Ok(())
 }
