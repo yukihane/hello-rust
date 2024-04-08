08b8a4f306c65ac2c12e934aeecec702df086774
yukihane <yukihane.feather@gmail.com>
2024-04-08 20:44:08 +0900

    anyhowでcontext追加

--- error-handling2/src/main.rs
+++ error-handling2/src/main.rs
@@ -1,8 +1,20 @@
-use error_handling2::run;
+pub mod wordcounter;
 
-fn main() {
-    if let Err(err) = run() {
-        eprintln!("Error: {:?}", err);
-        std::process::exit(1);
+use std::env;
+use std::error::Error;
+use std::fs::File;
+
+use anyhow::Context;
+
+use crate::wordcounter::count_words;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    for filename in env::args().skip(1).collect::<Vec<String>>() {
+        let mut reader = File::open(&filename).context(format!("unable to opne '{}'", filename))?;
+
+        let wordcount =
+            count_words(&mut reader).context(format!("unable to count words in '{}'", filename))?;
+        println!("{} {}", wordcount, filename);
     }
+    Ok(())
 }
