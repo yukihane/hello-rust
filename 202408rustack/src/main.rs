1f8613ea1ee5c269f5c1e3642adbebda98c347e8
yukihane <yukihane.feather@gmail.com>
2024-12-27 00:28:01 +0900

    ?

--- 202408rustack/src/main.rs
+++ 202408rustack/src/main.rs
@@ -1,3 +1,16 @@
 fn main() {
-    println!("Hello, world!");
+    let mut stack = vec![];
+
+    stack.push(42);
+    stack.push(36);
+
+    add(&mut stack);
+
+    println!("stack: {stack:?}");
+}
+
+fn add(stack: &mut Vec<i32>) {
+    let lhs = stack.pop().unwrap();
+    let rhs = stack.pop().unwrap();
+    stack.push(lhs + rhs);
 }
