b481f6c0fbf84618554bfbc59fff4211a9f1ae07
yukihane <yukihane.feather@gmail.com>
2024-12-29 06:14:40 +0900

    シェルピンスキー三角形の削除

--- 202412Game-Development-with-Rust-and-WebAssembly/walk-the-dog/src/lib.rs
+++ 202412Game-Development-with-Rust-and-WebAssembly/walk-the-dog/src/lib.rs
@@ -32,69 +32,6 @@ struct Cell {
 struct Sheet {
     frames: HashMap<String, Cell>,
 }
-fn midpoint(point_1: (f64, f64), point_2: (f64, f64)) -> (f64, f64) {
-    ((point_1.0 + point_2.0) / 2.0, (point_1.1 + point_2.1) / 2.0)
-}
-
-fn sierpinski(
-    context: &web_sys::CanvasRenderingContext2d,
-    points: [(f64, f64); 3],
-    color: (u8, u8, u8),
-    depth: u8,
-) {
-    draw_triangle(&context, points, color);
-    let depth = depth - 1;
-    let [top, left, right] = points;
-    if depth > 0 {
-        let mut rng = thread_rng();
-        let next_color = (
-            rng.gen_range(0..255),
-            rng.gen_range(0..255),
-            rng.gen_range(0..255),
-        );
-
-        let left_middle = midpoint(top, left);
-        let right_middle = midpoint(top, right);
-        let bottom_middle = midpoint(left, right);
-        sierpinski(
-            &context,
-            [top, left_middle, right_middle],
-            next_color,
-            depth,
-        );
-        sierpinski(
-            &context,
-            [left_middle, left, bottom_middle],
-            next_color,
-            depth,
-        );
-        sierpinski(
-            &context,
-            [right_middle, bottom_middle, right],
-            next_color,
-            depth,
-        );
-    }
-}
-
-fn draw_triangle(
-    context: &web_sys::CanvasRenderingContext2d,
-    points: [(f64, f64); 3],
-    color: (u8, u8, u8),
-) {
-    let color_str = format!("rgb({},{},{})", color.0, color.1, color.2);
-    context.set_fill_style_str(&color_str);
-
-    let [top, left, right] = points;
-    context.move_to(top.0, top.1);
-    context.begin_path();
-    context.line_to(left.0, left.1);
-    context.line_to(right.0, right.1);
-    context.line_to(top.0, top.1);
-    context.close_path();
-    context.stroke();
-    context.fill();
-}
 
 // This is like the `main` function, except for JavaScript.
 #[wasm_bindgen(start)]
@@ -139,13 +76,6 @@ pub fn main_js() -> Result<(), JsValue> {
 
         success_rx.await;
         context.draw_image_with_html_image_element(&image, 0.0, 0.0);
-
-        sierpinski(
-            &context,
-            [(300.0, 0.0), (0.0, 600.0), (600.0, 600.0)],
-            (0, 255, 0),
-            5,
-        );
 
         context.draw_image_with_html_image_element(&image, 0.0, 0.0);
         let json = fetch_json("walk_the_dog_assets-0.0.7/sprite_sheets/rhb.json")
