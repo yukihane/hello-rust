88e5ff63c1a634273cf9b625555a7f2010676754
yukihane <yukihane.feather@gmail.com>
2024-12-28 14:32:58 +0900

    imageのloadを待って後続処理実行

--- 202412Game-Development-with-Rust-and-WebAssembly/walk-the-dog/src/lib.rs
+++ 202412Game-Development-with-Rust-and-WebAssembly/walk-the-dog/src/lib.rs
@@ -95,22 +95,26 @@ pub fn main_js() -> Result<(), JsValue> {
         .dyn_into::<web_sys::CanvasRenderingContext2d>()
         .unwrap();
 
-    let image = web_sys::HtmlImageElement::new().unwrap();
+    wasm_bindgen_futures::spawn_local(async move {
+        let (success_tx, success_rx) = futures::channel::oneshot::channel::<()>();
+        let image = web_sys::HtmlImageElement::new().unwrap();
 
-    let callback = Closure::once(|| {
-        web_sys::console::log_1(&JsValue::from_str("loaded"));
+        let callback = Closure::once(|| {
+            success_tx.send(());
+        });
+        image.set_onload(Some(callback.as_ref().unchecked_ref()));
+        image.set_src("walk_the_dog_assets-0.0.7/resized/rhb/Idle (1).png");
+
+        success_rx.await;
+        context.draw_image_with_html_image_element(&image, 0.0, 0.0);
+
+        sierpinski(
+            &context,
+            [(300.0, 0.0), (0.0, 600.0), (600.0, 600.0)],
+            (0, 255, 0),
+            5,
+        );
     });
-    image.set_onload(Some(callback.as_ref().unchecked_ref()));
-    callback.forget();
-    image.set_src("walk_the_dog_assets-0.0.7/resized/rhb/Idle (1).png");
-    context.draw_image_with_html_image_element(&image, 0.0, 0.0);
-
-    sierpinski(
-        &context,
-        [(300.0, 0.0), (0.0, 600.0), (600.0, 600.0)],
-        (0, 255, 0),
-        5,
-    );
 
     Ok(())
 }
