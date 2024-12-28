0a962f13f56276c463bef7566658e72041c64e55
yukihane <yukihane.feather@gmail.com>
2024-12-29 06:11:56 +0900

    2.4 2人目のキャラクター描画まで

--- 202412Game-Development-with-Rust-and-WebAssembly/walk-the-dog/src/lib.rs
+++ 202412Game-Development-with-Rust-and-WebAssembly/walk-the-dog/src/lib.rs
@@ -154,6 +154,41 @@ pub fn main_js() -> Result<(), JsValue> {
 
         let sheet: Sheet = serde_wasm_bindgen::from_value(json)
             .expect("Could not convert rhb.json into a Sheet structure");
+
+        let (success_tx, success_rx) = futures::channel::oneshot::channel::<Result<(), JsValue>>();
+        let success_tx = Rc::new(Mutex::new(Some(success_tx)));
+        let error_tx = Rc::clone(&success_tx);
+        let image = web_sys::HtmlImageElement::new().unwrap();
+
+        let callback = Closure::once(move || {
+            if let Some(success_tx) = success_tx.lock().ok().and_then(|mut opt| opt.take()) {
+                success_tx.send(Ok(()));
+            }
+        });
+        let error_callback = Closure::once(move |err| {
+            if let Some(error_tx) = error_tx.lock().ok().and_then(|mut opt| opt.take()) {
+                error_tx.send(Err(err));
+            }
+        });
+
+        image.set_onload(Some(callback.as_ref().unchecked_ref()));
+        image.set_onerror(Some(error_callback.as_ref().unchecked_ref()));
+        image.set_src("walk_the_dog_assets-0.0.7/sprite_sheets/rhb.png");
+
+        success_rx.await;
+
+        let sprite = sheet.frames.get("Run (1).png").expect("Cell not found");
+        context.draw_image_with_html_image_element_and_sw_and_sh_and_dx_and_dy_and_dw_and_dh(
+            &image,
+            sprite.frame.x.into(),
+            sprite.frame.y.into(),
+            sprite.frame.w.into(),
+            sprite.frame.h.into(),
+            300.0,
+            300.0,
+            sprite.frame.w.into(),
+            sprite.frame.h.into(),
+        );
     });
 
     Ok(())
